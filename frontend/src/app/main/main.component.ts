b6e594bfa71cc3dd93047bade722da19a7ea0ebe
Xabiito1 <xpineiroperez@gmail.com>
2023-10-03 12:50:20 +0200

    timer buttons updated

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -6,4 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  jornadaEstado: 'iniciar' | 'pausar' | 'reanudar' | 'finalizar' = 'iniciar';
+
+  mostrarIniciarJornada: boolean = true;
+
+  iniciarJornada(): void {
+    this.jornadaEstado = 'pausar';
+    this.mostrarIniciarJornada = false;
+  }
+
+  pausarJornada(): void {
+    this.jornadaEstado = 'reanudar';
+  }
+
+  reanudarJornada(): void {
+    this.jornadaEstado = 'pausar';
+  }
+
+  finalizarJornada(): void {
+    this.jornadaEstado = 'iniciar';
+    this.mostrarIniciarJornada = true;
+  }
 }
