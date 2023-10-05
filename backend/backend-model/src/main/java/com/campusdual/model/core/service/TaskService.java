3e8e1b985a8c7a3cb9441ee8c91c6523a3e03d53
Gabriel Motto <buderball11@gmail.com>
2023-10-05 09:46:14 +0200

    Query changes on tasks

--- backend/backend-model/src/main/java/com/campusdual/model/core/service/TaskService.java
+++ backend/backend-model/src/main/java/com/campusdual/model/core/service/TaskService.java
@@ -40,4 +40,9 @@ public class TaskService implements ITaskService {
     public EntityResult taskDelete(Map<?, ?> keyMap) {
         return this.daoHelper.delete(this.taskDao, keyMap);
     }
+
+    @Override
+    public EntityResult taskTimerQuery(Map<?, ?> keyMap, List<?> attrList) {
+        return this.daoHelper.query(this.taskDao, keyMap, attrList, "taskTimer");
+    }
 }
