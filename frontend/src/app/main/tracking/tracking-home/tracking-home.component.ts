cbeedee3e8cafb93a1ad75c4245695c60779573d
Xabiito1 <xpineiroperez@gmail.com>
2023-10-05 08:18:14 +0200

    tracking module scss

--- frontend/src/app/main/tracking/tracking-home/tracking-home.component.ts
+++ frontend/src/app/main/tracking/tracking-home/tracking-home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tracking-home',
   templateUrl: './tracking-home.component.html',
-  styleUrls: ['./tracking-home.component.css']
+  styleUrls: ['./tracking-home.component.scss']
 })
 export class TrackingHomeComponent implements OnInit {
 
