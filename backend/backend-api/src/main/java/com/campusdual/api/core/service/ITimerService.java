8b34cd6309471906b47514b086dcca05118b6891
angel <jjjk>
2023-10-06 10:58:24 +0200

    Insert authentication implemented in backend

--- backend/backend-api/src/main/java/com/campusdual/api/core/service/ITimerService.java
+++ backend/backend-api/src/main/java/com/campusdual/api/core/service/ITimerService.java
@@ -1,13 +1,14 @@
 package com.campusdual.api.core.service;
 
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 
 import java.util.List;
 import java.util.Map;
 
 public interface ITimerService {
-    public EntityResult timerQuery(Map<?, ?> keyMap, List<?> attrList);
-    public EntityResult timerInsert(Map<?, ?> attrMap);
-    public EntityResult timerUpdate(Map<?, ?> attrMap, Map<?, ?> keyMap);
-    public EntityResult timerDelete(Map<?, ?> keyMap);
+    public EntityResult timerQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
+    public EntityResult timerInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
+    public EntityResult timerUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+    public EntityResult timerDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
 }
--- backend/backend-model/src/main/java/com/campusdual/model/core/service/TimerService.java
+++ backend/backend-model/src/main/java/com/campusdual/model/core/service/TimerService.java
@@ -7,6 +7,8 @@ import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,22 +24,24 @@ public class TimerService implements ITimerService {
 
 
     @Override
-    public EntityResult timerQuery(Map<?, ?> keyMap, List<?> attrList) {
+    public EntityResult timerQuery(Map<String, Object> keyMap, List<String> attrList) {
         return this.daoHelper.query(this.timerDao, keyMap, attrList);
     }
 
     @Override
-    public EntityResult timerInsert(Map<?, ?> attrMap) {
+    public EntityResult timerInsert(Map<String, Object> attrMap) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        attrMap.put(timerDao.USER_, authentication.getName());//NOMBRE ususario
         return this.daoHelper.insert(this.timerDao, attrMap);
     }
 
     @Override
-    public EntityResult timerUpdate(Map<?, ?> attrMap, Map<?, ?> keyMap) {
+    public EntityResult timerUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) {
         return this.daoHelper.update(this.timerDao, attrMap, keyMap);
     }
 
     @Override
-    public EntityResult timerDelete(Map<?, ?> keyMap) {
+    public EntityResult timerDelete(Map<String, Object> keyMap) {
         return this.daoHelper.delete(this.timerDao, keyMap);
     }
 }
