86c9e4e2f9f332951850ac5656c959e504b60195
Xabier <xpineiroperez@gmail.com>
2023-10-10 13:42:45 +0200

    changed aside icons

--- frontend/src/app/shared/app.menu.config.ts
+++ frontend/src/app/shared/app.menu.config.ts
@@ -2,8 +2,8 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   //{ id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'tracking', name: 'TRACKING', route: '/main/tracking', icon: 'timeline' },
-  { id: 'projects', name: 'PROJECTS', route: '/main/projects', icon: 'code' },
-  { id: 'tasks', name: 'TASKS', route: '/main/tasks', icon: 'list' },
+  { id: 'tracking', name: 'TRACKING', route: '/main/tracking', icon: 'alarm' },
+  { id: 'projects', name: 'PROJECTS', route: '/main/projects', icon: 'folder' },
+  { id: 'tasks', name: 'TASKS', route: '/main/tasks', icon: 'playlist_add_check' },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
