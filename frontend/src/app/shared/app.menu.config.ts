b3db92b4c26d2122cb5e332731e13f096e2ddbcf
Xabier <xpineiroperez@gmail.com>
2023-10-11 08:20:28 +0200

    hotfix

--- frontend/src/app/shared/app.menu.config.ts
+++ frontend/src/app/shared/app.menu.config.ts
@@ -5,6 +5,6 @@ export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'tracking', name: 'TRACKING', route: '/main/tracking', icon: 'alarm' },
   { id: 'projects', name: 'PROJECTS', route: '/main/projects', icon: 'folder' },
   { id: 'tasks', name: 'TASKS', route: '/main/tasks', icon: 'playlist_add_check' },
-  { id: 'record', name: 'RECORD', route: '/main/record', icon: 'playlist_add_check' },
+  { id: 'record', name: 'RECORD', route: '/main/record', icon: 'history' },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
