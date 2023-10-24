4403a8bcbdea65d04c380781b6dee6580b8e7f19
angel <jjjk>
2023-10-24 13:50:40 +0200

    Time tracker showing time for the current task.

--- frontend/src/app/shared/timer-controls/timer-controls.component.ts
+++ frontend/src/app/shared/timer-controls/timer-controls.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Injector, ViewChild } from '@angular/core';
 import { OComboComponent, OntimizeService } from 'ontimize-web-ngx';
+
 
 @Component({
   selector: 'app-timer-controls',
@@ -11,21 +12,25 @@ export class TimerControlsComponent implements OnInit {
 
   isStarted: boolean = false;
   selectedTaskValue: any;
+  startTime: number;
+  temporizadorInterval: any;
 
   protected service: OntimizeService;
 
-  @ViewChild('taskCombo', {static: true})taskCombo : OComboComponent;
+  @ViewChild('taskCombo', { static: true }) taskCombo: OComboComponent;
+  @ViewChild('crono', { static: true }) crono: ElementRef;
 
   constructor(protected injector: Injector) {
     this.service = this.injector.get(OntimizeService);
   }
-  
+
   ngAfterViewInit(): void {
   }
-  
+
   ngOnInit() {
     this.configureService();
     this.getLastOpen();
+    this.startCrono();
 
     const timerStatus = localStorage.getItem('timerStatus');
     if (timerStatus === 'true') {
@@ -46,13 +51,14 @@ export class TimerControlsComponent implements OnInit {
 
   startTimer() {
     if (this.service !== null) {
-      const values = {T_ID: this.getComboValue()};
+      const values = { T_ID: this.getComboValue() };
       this.service.insert(values, "timer").subscribe(resp => {
         if (resp.code === 0) {
           this.selectedTaskValue = this.getComboValue();
           localStorage.setItem('timerStatus', 'true');
           localStorage.setItem('selectedTaskValue', JSON.stringify(this.selectedTaskValue));
           this.iniciarJornada();
+          this.startCrono();
         } else {
           //TODO: Mostrar error
         }
@@ -61,19 +67,63 @@ export class TimerControlsComponent implements OnInit {
   }
 
 
-  stopTimer(){
+  stopTimer() {
     if (this.service !== null) {
       const values = {};
       const filter = {};
       this.service.update(filter, values, "close").subscribe(resp => {
         if (resp.code === 0) {
+
         } else {
           //TODO: Mostrar error
         }
       });
       localStorage.setItem('timerStatus', 'false');
       this.finalizarJornada();
+      clearInterval(this.temporizadorInterval);
+
     }
+  }
+
+  //meter en el otro método
+  startCrono() { 
+    
+    if (this.service != null) {
+      const filter = {};
+      const columns = ['TM_START_TIME'];
+      this.service.query(filter, columns, 'openTimer').subscribe(resp => {
+        if (resp.code === 0) {
+          if (resp.data.length>0){
+          this.startTime = resp.data[0].TM_START_TIME;
+          this.iniciarContador();
+          }
+          
+        } else {
+
+        }
+      });
+    }
+  }
+
+  iniciarContador() {
+
+    let self = this;
+
+    const actualizarTiempo = () => {
+
+      let currentDate = new Date().getTime();
+      let diff = currentDate - self.startTime;
+      console.log(diff);
+      let segundos = Math.floor(diff / 1000) % 60;
+      let minutos = Math.floor((diff/1000)/ 60);
+
+    
+      const tiempoFormateado = `${String(minutos).padStart(2, "0")}:${String(segundos).padStart(2, "0")}`;
+
+      self.crono.nativeElement.innerHTML = `${tiempoFormateado}`;
+    }
+
+    this.temporizadorInterval = setInterval(actualizarTiempo, 1000);
   }
 
   getLastOpen() {
@@ -91,14 +141,16 @@ export class TimerControlsComponent implements OnInit {
       });
     }
   }
-  
+
   getComboValue() {
     return this.taskCombo.getValue();
   }
 
+
+
   iniciarJornada(): void {
     this.isStarted = true;
-  }  
+  }
 
   finalizarJornada(): void {
     this.isStarted = false;
