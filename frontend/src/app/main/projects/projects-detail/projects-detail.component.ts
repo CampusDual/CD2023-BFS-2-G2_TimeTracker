b4ec746e64938415a37a1bc75ce0d628531c3258
Gabriel Motto <buderball11@gmail.com>
2023-11-10 10:17:46 +0100

    Open tasks details on project tasks table and null filter option added

--- frontend/src/app/main/projects/projects-detail/projects-detail.component.ts
+++ frontend/src/app/main/projects/projects-detail/projects-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { OTableComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-projects-detail',
@@ -7,8 +9,17 @@ import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 })
 export class ProjectsDetailComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('tasksTable', { static: false }) tasksTable: OTableComponent;
+
+  constructor(private router: Router) {}
 
   ngOnInit() { }
 
+  public openTasksDetailsSelected() {
+    let selectedItem = this.tasksTable.getSelectedItems();
+    if (selectedItem.length === 1) {
+      let taskId = selectedItem[0]['T_ID'];
+      this.router.navigate(['main/tasks/' + taskId]);
+    }
+  }
 }
--- frontend/src/app/main/tasks/tasks-routing.module.ts
+++ frontend/src/app/main/tasks/tasks-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   {
    path:":T_ID",
     component: TasksDetailComponent
-   }
+  }
 
 ];
 
