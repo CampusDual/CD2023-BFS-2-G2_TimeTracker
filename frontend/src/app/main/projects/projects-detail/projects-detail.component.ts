4c25bd135c67f15ec1429e176f3c9d18feb2b4ed
angel <jjjk>
2023-11-16 11:48:36 +0100

    Project progress % displayed on project details

--- frontend/src/app/main/projects/projects-detail/projects-detail.component.ts
+++ frontend/src/app/main/projects/projects-detail/projects-detail.component.ts
@@ -1,7 +1,7 @@
 import { TemplateBinding } from '@angular/compiler';
 import { Component, OnInit, Injector, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { OTableComponent, OTextInputComponent, OntimizeService } from 'ontimize-web-ngx';
+import { OIntegerInputComponent, OPercentInputComponent, OTableComponent, OTextInputComponent, OntimizeService } from 'ontimize-web-ngx';
 import { PieChartConfiguration } from 'ontimize-web-ngx-charts';
 
 @Component({
@@ -13,8 +13,13 @@ export class ProjectsDetailComponent implements OnInit {
 
   @ViewChild('tasksTable', { static: false }) tasksTable: OTableComponent;
   @ViewChild('projectTime', { static: true }) projectTime: OTextInputComponent;
+  @ViewChild('maxHours', { static: true }) maxHours: OIntegerInputComponent;
+  @ViewChild('progressPercent', { static: true }) progressPercent: OPercentInputComponent;
+
   p_id: any;
   tasksChartParameters: PieChartConfiguration;
+  totaltime: number;
+
 
   constructor(private router: Router) {
     this.tasksChartParameters = new PieChartConfiguration;
@@ -35,13 +40,25 @@ export class ProjectsDetailComponent implements OnInit {
   viewData(dataPT) {
     console.log(dataPT);
     this.p_id = dataPT.P_ID;
-    let projectTotalTime = dataPT.PROJECT_TOTAL_TIME;
+    this.totaltime = dataPT.PROJECT_TOTAL_TIME;
 
-    let minutes = this.addZero(projectTotalTime % 60);
-    let hours =  this.addZero(Math.floor(projectTotalTime / 60));
+    let minutes = this.addZero(this.totaltime % 60);
+    let hours =  this.addZero(Math.floor(this.totaltime / 60));
 
     this.projectTime.setValue(`${hours}:${minutes}`);
   }
+
+  getProgress(){
+    let progress = ((this.totaltime/60) * 100 / this.maxHours.getValue())/100;
+    if (progress > 1){
+      this.progressPercent.elementRef.nativeElement.style.color = "red";
+    } else {
+      this.progressPercent.elementRef.nativeElement.style.color = "#333";
+    }
+
+    return progress;
+  }
+
 
   addZero(num) {
     if (num < 10) {
